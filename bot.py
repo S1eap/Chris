475ff08065bd52e984bdf6f419708ce52218ddfd
ChrisKaspersky <fckthscty@gmail.com>
2017-06-19 21:45:35 +0300

    .

--- bot.py
+++ bot.py
@@ -1,17 +1,16 @@
 import logging #Библиотека для ведения логов, потом пригодится
-import vk #Модуль для доступа к API
 import vk.exceptions #Модуль с описанием исключений связанных с API
 import time
+
+logging.basicConfig(format='[# %(levelname)-10s [%(asctime)s]  %(message)s', level=logging.INFO)
+
 tokenfile=open('token.txt','r')
 #/////logss=open('loggs.txt','w')
-
-
 
 quest=['Привет','привет','Как дела?','как дела?','Как дела','как дела']
 ans=['Привет','Привет','Хорошо, как у тебя?','Хорошо, как у тебя','Хорошо, как у тебя','Хорошо, как у тебя','Хорошо, как у тебя','Хорошо, как у тебя']
 
-
-def anser(vopr):
+def answer(vopr):
 	for i in range (0,len(quest)+1):
 		if i==len(quest):
 			break
@@ -19,41 +18,26 @@ def anser(vopr):
 			return ans[i]
 	if i==len(quest):
 		return 'Я не знаю такого вопроса'
-	
-
-
-
-
-
-
-
-logging.basicConfig(format='[# %(levelname)-10s [%(asctime)s]  %(message)s', level=logging.INFO) #Настройка логов, на будущее
 
 logging.info("Сервис запущен") # Вывод инфы по авторизации
 logging.info("Авторизация...")
 token=tokenfile.readline()
 
-try: # Попытка авторизации
-       session = vk.Session(access_token=token)
-       api = vk.API(session) #Создаём обьект класса API
+try:
+    session = vk.Session(access_token=token)
+    api = vk.API(session) #Создаём обьект класса API
 except vk.exceptions.VkAuthError: # Если ошибка заключается в авторизации
         logging.info('Авторизация не удалась: неверный логин или пароль')
 
 logging.info("Начат приём сообщений")
-prev='blablablaalbalbalb' #Создаем предыдущее сообщение 
+
+last_message = ''
 while True:
-	last=api.messages.get(out=0,count=1,)
-	if prev==last: 										#Если последнее прочитанное сообщение равно предыдущему, то мы проверяем через 15 сек. Также єто нужно чтобы бот не отвечал по 20 раз на 1 сообщение
-		time.sleep(5)
-	else: 												#Если последнее прочитанное сообщение не равно предыдущему, то мы проверяем через 3 сек (в случае "оживленной" переписки")
-		#print(last_message)
-		print(last[1]['body'])
+	if last_message != api.messages.get(out=0,count=1):
+		last_message = api.messages.get(out=0,count=1)
+		print(last_message[1]['body'])
 
-		api.messages.send(user_id=last[1]['uid'], message=anser(last[1]['body'])) #Отправка ответа
-		#/////print('Ответ: '+anser(last[1]['body'])
-		#/////logss.write('Вопрос: '+last[1]['body']+'    Ответ: '+anser(last[1]['body']) 
-
-		time.sleep(3)
-	prev=last
+		api.messages.send(user_id=last_message[1]['uid'], message=answer(last_message[1]['body'])) #Отправка ответа
+	time.sleep(1)
 
 
